8e77535fc3a7f11f53901f32722315a302efd103
Lars Gröninger <lars.groninger@gmail.com>
2023-10-13 17:41:14 +0200

    Add script to clone repos

---
+++ repos/clone_repos.py
@@ -0,0 +1,14 @@
+import subprocess
+
+repos = [
+    'https://github.com/psf/black.git',
+    'https://github.com/tiangolo/fastapi.git',
+    'https://github.com/pallets/flask.git',
+    'https://github.com/pandas-dev/pandas.git',
+    'https://github.com/TheAlgorithms/Python.git',
+    'https://github.com/scikit-learn/scikit-learn.git',
+    'https://github.com/scrapy/scrapy.git'
+]
+
+for repo in repos:
+    subprocess.run(f'git clone {repo}')
