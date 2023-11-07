d1e7a515b26931b37dac4f96f1a25690beb71129
Lars Gröninger <lars.groninger@gmail.com>
2023-11-07 17:04:15 +0100

    Update repos from which data is collected

--- repos/clone_repos.py
+++ repos/clone_repos.py
@@ -1,14 +1,16 @@
 import subprocess
 
 repos = [
+    'https://github.com/apache/airflow.git',
     'https://github.com/psf/black.git',
+    'https://github.com/httpie/cli.git',
     'https://github.com/tiangolo/fastapi.git',
     'https://github.com/pallets/flask.git',
     'https://github.com/pandas-dev/pandas.git',
+    'https://github.com/python-poetry/poetry.git'
     'https://github.com/TheAlgorithms/Python.git',
     'https://github.com/scikit-learn/scikit-learn.git',
     'https://github.com/scrapy/scrapy.git',
-    'https://github.com/Textualize/rich.git'
 ]
 
 for repo in repos:
