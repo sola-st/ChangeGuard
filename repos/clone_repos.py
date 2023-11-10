5a1a3af408c1ed65bdb597b63ba64aab9a46c4be
Lars Gröninger <lars.groninger@gmail.com>
2023-11-10 13:56:22 +0100

    Add missing comma to clone_repos.py

--- repos/clone_repos.py
+++ repos/clone_repos.py
@@ -7,7 +7,7 @@ repos = [
     'https://github.com/tiangolo/fastapi.git',
     'https://github.com/pallets/flask.git',
     'https://github.com/pandas-dev/pandas.git',
-    'https://github.com/python-poetry/poetry.git'
+    'https://github.com/python-poetry/poetry.git',
     'https://github.com/TheAlgorithms/Python.git',
     'https://github.com/scikit-learn/scikit-learn.git',
     'https://github.com/scrapy/scrapy.git',
