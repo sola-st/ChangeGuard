a691247eede76805e4044e159c1a74cf333ba20d
Lars Gröninger <lars.groninger@gmail.com>
2023-11-29 16:28:50 +0100

    Add setup.py to LExecutorCC

---
+++ LExecutorCC/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("../README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lexecutor",
+    version="0.0.1",
+    author="Michael Pradel",
+    author_email="michael@binaervarianz.de",
+    description="Learning-guided execution",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/michaelpradel/LExecutor",
+    project_urls={
+        "Bug Tracker": "https://github.com/michaelpradel/LExecutor/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "lexecutor"},
+    packages=setuptools.find_packages(where="lexecutor"),
+    python_requires=">=3.6",
+    install_requires=[
+        'libcst',
+        'pandas',
+        'torch',
+        'tables',
+    ],
+)
