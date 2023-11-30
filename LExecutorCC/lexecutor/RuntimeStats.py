022aa4736e01a9dab17aa8eda733293602685d44
Lars Gröninger <lars.groninger@gmail.com>
2023-11-30 12:58:03 +0100

    Fix Runtimestats printing IID instead of line number

--- LExecutorCC/lexecutor/RuntimeStats.py
+++ LExecutorCC/lexecutor/RuntimeStats.py
@@ -51,7 +51,7 @@ class RuntimeStats:
         logger.info(f"Covered iids: {len(self.covered_iids)}")
         logger.info(f"Total uses: {self.total_uses}")
         logger.info(f"Guided uses : {self.guided_uses}/{self.total_uses}")
-        logger.info(f"Executed lines: {self.executed_lines}")
+        logger.info(f"Executed lines: {[self.iids.line(iid) for iid in self.executed_lines]}")
 
     def _save_summary_metrics(self, file, predictor_name, execution_time):
         if write_metrics:
