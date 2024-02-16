009130a7aee0c93de277766ebb138bff4d62cf70
Lars Gröninger <lars.groninger@gmail.com>
2024-02-16 12:34:17 +0100

    Add __call__ back to DummyObject

--- LExecutorCC/lexecutor/ValueAbstraction.py
+++ LExecutorCC/lexecutor/ValueAbstraction.py
@@ -218,8 +218,8 @@ class DummyObject(Exception):
     def __bytes__(self):
         return bytes(self.str, "utf-8")
 
-    # def __call__(self, *args, **kwargs):
-    #     return DummyObject()
+    def __call__(self, *args, **kwargs):
+        return DummyObject()
 
     # def __ceil__(self):
     #     pass
